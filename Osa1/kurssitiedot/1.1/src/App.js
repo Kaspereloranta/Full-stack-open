9b9a702e3a23b09de98fd54917a971a4c175dcc4
Kasper Eloranta <kasper.eloranta@tuni.fi>
2022-08-31 19:40:34 +0300

    tehtävä 1.5

--- Osa1/kurssitiedot/1.1/src/App.js
+++ Osa1/kurssitiedot/1.1/src/App.js
@@ -3,7 +3,7 @@ const Header = (props) => {
   return (
     <div>
       <p>
-        {props.name}
+        {props.course}
       </p>
     </div>
   )
@@ -48,29 +48,30 @@ const Total = (props) => {
 
 const App = () => {
   
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <h1><Header name={course} /></h1>
+      <h1><Header course={course.name} /></h1>
       <p>
-      <Content parts={parts}/>
+      <Content parts={course.parts}/>
       </p>
-      <Total parts={parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
   }
