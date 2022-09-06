884ad456586516ba01635563c83f7df142885432
Kasper Eloranta <kasper.eloranta@tuni.fi>
2022-09-06 21:30:09 +0300

    tehtävä 2.8

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -2,33 +2,42 @@ import { useState } from 'react'
 
 const Number=(props) => {
   return(
-    <p>{props.person.name}</p>
+    <p>{props.person.name} {props.person.number}</p>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas' ,
+      number: '040-1231244'}
+    ]) 
   const [newName, setNewName] = useState('')
-
+  const [newNumber, setNewNumber] = useState('')
   const addNumber = (event) => {
     event.preventDefault()
-    const NumberObject = {
-      name: newName
+    const PersonObject = {
+      name: newName,
+      number: newNumber
     }
 
     if((persons.map(person => person.name)).includes(newName)){
-      alert(newName + " is already added to phonebook")
+      alert(`${newName} is already added to phonebook`)
     }
     else{
-      setPersons(persons.concat(NumberObject))
+      setPersons(persons.concat(PersonObject))
+      console.log(persons[2])
       setNewName('')
+      setNewNumber('')   
     }
   }
 
-  const handleNumberChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -36,18 +45,21 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addNumber}>
         <div>
-          name: <input value={newName} onChange={handleNumberChange}  />
+          name: <input value={newName} onChange={handleNameChange}  />
         </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <ul>
       {persons.map(person => 
           <Number key={person.name} person={person}></Number>)}
       </ul>
-      <Number person={persons}></Number>
+       <Number person={persons}></Number>
     </div>
   )
 
