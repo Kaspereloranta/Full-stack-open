f4c234cdd24c26bb6e58e36eca4329b156484ac1
Kaspereloranta <kasper.eloranta@outlook.com>
2022-09-08 13:18:41 +0300

    tehtävä 2.11

--- Osa2/puhelinluettelo/src/index.js
+++ Osa2/puhelinluettelo/src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+/*
+import reportWebVitals from './reportWebVitals';
+*/
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,3 +21,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
