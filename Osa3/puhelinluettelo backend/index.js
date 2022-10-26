c88d49b3dae9917b934c6187b7ccded77b09e858
Kasper Eloranta <kasper.eloranta@tuni.fi>
2022-10-26 11:07:46 +0300

    tehtävä 3.16

--- Osa3/puhelinluettelo backend/index.js
+++ Osa3/puhelinluettelo backend/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+app.use(express.static('build'))
 app.use(express.json())
 const cors = require('cors')
 app.use(cors())
-app.use(express.static('build'))
 const Person = require('./models/person')
 
 let persons = [
@@ -62,10 +62,17 @@ app.get('/api/persons/:id', (request, response) => {
   })
 */
 //mongoDB
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).send({ error: 'unknown endpoint' }).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 // mongoDB
@@ -86,6 +93,21 @@ app.delete('/api/persons/:id', (request, response, next) => {
     response.status(204).end()
 })
 */
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
