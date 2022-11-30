014f506886bd89f15d7b6ae75a33bbe9d6506df0
Kasper Eloranta <kasper.eloranta@tuni.fi>
2022-11-30 17:17:57 +0200

    tehtävä 4.4

--- Osa4/blogilista/utils/list_helper.js
+++ Osa4/blogilista/utils/list_helper.js
@@ -2,7 +2,15 @@ const dummy = (blogs) => {
     let a = 1
     return a
 }
-  
+ 
+const totalLikes = (blogs) => {
+    var totalLikes = blogs.reduce(function(sum,blog) {
+        return sum + blog.likes
+    }, 0)
+    return totalLikes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
