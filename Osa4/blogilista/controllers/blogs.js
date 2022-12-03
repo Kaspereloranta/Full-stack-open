db08dec943026c6a145c1501974384d34c632af3
Kasper Eloranta <kasper.eloranta@tuni.fi>
2022-12-03 15:39:58 +0200

    at least tasks 4.15, 4.17, 4.18, 4.19

--- Osa4/blogilista/controllers/blogs.js
+++ Osa4/blogilista/controllers/blogs.js
@@ -1,9 +1,14 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
 
 blogsRouter.get('/', async (request, response) => { 
-  const blogs = await Blog.find({})
-  response.json(blogs)
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.get('/:id', async (request, response) => {
@@ -15,17 +20,39 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async(request, response,) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  let user = await User.findById(body.userId)
+
+  if(!user){
+    user = await User.findOne()
+  }
 
   const blog = new Blog({
-    title: body.title, 
+    title: body.title,
     author: body.author,
-    url: body.url, 
-    likes: body.likes || 0
+    url: body.url,
+    likes: body.likes || 0,
+    user: user._id 
   })
-  const savedNote = await blog.save()
-  response.json(savedNote)
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
