1e88a57654a8c8558cead104bf9ddc07fde36daa
Kasper Eloranta <kasper.eloranta@tuni.fi>
2022-12-20 19:47:43 +0200

    task 5.18

--- Osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ Osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -4,7 +4,7 @@ describe('Blog app', function() {
     const user = {
       name: 'Kasper Eoranta',
       username: 'KasperE',
-      password: 'testi'
+      password: 'test'
     }
     cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3001')
@@ -15,5 +15,24 @@ describe('Blog app', function() {
     cy.contains('password')
     cy.contains('log in')
   })
-})
 
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('KasperE')
+      cy.get('#password').type('test')
+      cy.get('#login-button').click()
+    })
+
+    it('fails with wrong credenttials', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('KasperE')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.get('.error').should('contain','Wrong username or password')
+      cy.get('.error').and('have.css', 'color', 'rgb(255, 0, 0)')
+      cy.get('.error').and('have.css', 'border-style', 'solid')
+      cy.get('html').should('not.contain', 'KasperE logged') 
+    })
+  })
+})
